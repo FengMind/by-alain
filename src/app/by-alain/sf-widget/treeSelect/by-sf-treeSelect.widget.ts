078b72cbb6eab84d53c500c4bf17e2a45a7b8cea
FengMind <lxf@caiyoudata.com>
2018-08-18 12:08:45 +0800

    test

--- src/app/by-alain/sf-widget/treeSelect/by-sf-treeSelect.widget.ts
+++ src/app/by-alain/sf-widget/treeSelect/by-sf-treeSelect.widget.ts
@@ -15,7 +15,7 @@ const DEFAULTFORMAT = 'YYYY-MM-DD HH:mm:ss';
   <sf-item-wrap [id]="id" [schema]="schema" [ui]="ui" [showError]="showError" [error]="error" [showTitle]="schema.title">
     <nz-tree-select nzPlaceHolder="请选择" [(ngModel)]="i.category_id" [nzNodes]="categoryIdNodes" (ngModelChange)="categoryIdChange($event)"
       [nzDropdownMatchSelectWidth]="true" [nzSize]="'middle'" [nzDropdownStyle]="{ 'max-height': '400px','width':'100%'}">
-      </nz-tree-select>
+      </nz-tree-select>11
   </sf-item-wrap>
   `,
     preserveWhitespaces: false,
